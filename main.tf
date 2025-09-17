c85c3a305beafeced78b5cc5b39c0032d40ed52a
Shubham <sak2944@gmail.com>
2025-09-17 15:26:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "sham" {
+resource "aws_instance" "test" {
     ami = "ami-0f88e80871fd81e91"
     instance_type = "t2.micro"
     tags = {
